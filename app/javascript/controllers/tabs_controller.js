8c39fdf5a97877bcaaf9cdd79069dab987bd0524
Apple <apple@Apples-MacBook-Pro.local>
2026-01-11 21:32:26 +0530

    Remove debugger statement from tabs controller and update UI files

---
+++ app/javascript/controllers/tabs_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tabs"
+export default class extends Controller {
+  static targets = ["signIn", "signUp", "signInForm", "signUpForm"]
+  
+
+  
+  connect() {   
+    const button_style = ["rounded-md", "shadow-sm",  "border", "border-[#EEE]", "text-[#111]", "bg-white"]
+    this.signInTarget.addEventListener("click", (event)=> {
+      
+      event.target.classList.add(...button_style)
+      this.signUpTarget.classList.remove(...button_style)
+      this.signInFormTarget.classList.remove("hidden")
+      this.signUpFormTarget.classList.add("hidden")
+    
+    })
+    this.signUpTarget.addEventListener("click", (event)=> {
+      event.target.classList.add(...button_style)
+      this.signInTarget.classList.remove(...button_style)
+      this.signUpFormTarget.classList.remove("hidden")
+      this.signInFormTarget.classList.add("hidden")
+     
+      
+    }) 
+   }
+}
