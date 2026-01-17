54f4c92e2e78e5e6f937f162bbd7b06539a2a756
Apple <apple@Apples-MacBook-Pro.local>
2026-01-18 04:45:22 +0530

    Add calendars and appointments associations and automatic profile creation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   has_many :role_users, dependent: :destroy
   has_many :roles, through: :role_users
   has_one :user_profile, dependent: :destroy
+  has_many :calendars, dependent: :destroy
+  has_many :appointments, dependent: :destroy
   validates :email_address, presence: true, uniqueness: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   after_initialize :set_default_role, if: :new_record?
+  after_create :create_user_profile
 
   def super_admin?
     roles.exists?(name: "SuperAdmin")
@@ -28,5 +31,9 @@ class User < ApplicationRecord
   def set_default_role
     role = Role.find_by(name: "Client")
     self.roles << role if role
+  end
+
+  def create_user_profile
+    UserProfile.find_or_create_by(user_id: self.id)
   end
 end
