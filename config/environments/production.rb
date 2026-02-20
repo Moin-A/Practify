42eb98f1bb975c287edca953076833d2a2a22191
Apple <apple@Apples-MacBook-Pro.local>
2026-02-21 00:05:04 +0530

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :ses
-  config.action_mailer.ses_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: ENV.fetch("AWS_REGION", "ap-south-1")
