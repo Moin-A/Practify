996e8658dfeb1137ebcf57e3126a68934f1e59c1
Apple <apple@Apples-MacBook-Pro.local>
2026-02-21 10:56:10 +0530

    minor fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@opentok/client", to: "@opentok--client.js" # @2.32.1
 pin "process" # @2.1.0
-pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.13
+pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.13/src/index.js"
